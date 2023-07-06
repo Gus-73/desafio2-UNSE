add52a19adeb0c6c7d61fe8aaa9ce661c800e4d8
Gustavo Ariel Nuñez <gustavoarienunezl73@gmail.com>
2023-07-06 17:28:00 -0300

    Añadí otro alert en "enviar mensaje". index.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function bienvenida(){
    let Nombre = document.getElementById('nombre').value;
    alert(`Bienvenido/a ${Nombre}!!! \n Aquí encontrarás todo lo que estás buscando.`);
 }
+
+let alerta = document.getElementById('buttonMensaje');
+function enviarAlerta(alerta){ 
+    event.preventDefault();
+   window.alert(`Su mensaje ha sido enviado satisfactoriamente`);
+}
